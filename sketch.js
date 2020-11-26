86fbb51fb92e4b7f57eb284583638c43eba0ca94
chiara <chiara7.brambilla@mail.polimi.it>
2020-11-26 16:24:18 +0100

    base layout

--- sketch.js
+++ sketch.js
@@ -18,8 +18,8 @@ let alt = 1; //h dei rettangoli suono
 let i = 0; //regola ogni quanto cambia alt
 let p_coord = 0; //var coordinazione
 
-let n_trombetta = 0; //var piattaforma: quando alt!=1 viene incrementata
-let n_interazione = 0; //var utente usa la trobetta, preme bottone
+let feed_piattaforma = 0 ; //n_trombetta = 0; //var piattaforma: quando alt!=1 viene incrementata
+let input_utente = 200 //n_interazione = 0; //var utente usa la trobetta, preme bottone
 //se faccio ntrombetta/niterazione trovo la coordinazione
 
 /////////////////////////////////////////////////////////////////////////
@@ -37,6 +37,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(15); //rallenta
+
 //impostazioni riconoscimento vocale
   let continuous = true;//continua a registrare
   let interim = true;
@@ -70,8 +71,8 @@ function draw() {
   imageMode(CENTER); //per pittogrammi
   noStroke();
 
-  w = width/2;
-  h = height/8;
+  w = width / 20;
+    h = height / 50;
 
   //testo caratteristiche
   textFont('quicksand');
@@ -79,66 +80,65 @@ function draw() {
   textStyle(BOLD);
 
   //testo centrale
-  textSize(16);
-  fill('#877B85'); //4° colore PALETTE
-  text('PARTITA COOD O1', w, height / 11);
-  textSize(14);
-  fill('#B7AEB5'); //3° PALETTE
-  text('SQUADRA1-SQUADRA2', w, h);
+    textSize(16);
+    fill('#877B85'); //4° colore PALETTE
+    text('PARTITA COOD O1', w*10, h*5);
+    fill('#B7AEB5'); //3° PALETTE
+    textSize(13);
+    text('SQUADRA1-SQUADRA2', w*10, h*6.5);
 
+    //testo sotto
+    textSize(14);
+    textAlign(CORNER);
+    text('BONUS',w*1.2, h*43);
 
   //logo a destra
-  image(logor, width/11*9.8, height /9,logor.width/4.5,logor.height/4.5);
+  image(logor, w * 18.5, h*6, logor.width / 4.5, logor.height / 4.5);
   //freccia
-  image(freccia, width/11, height /9,freccia.width/6,freccia.height/6);
-
-  //testo sotto
-  text('COORDINAZIONE', w-30, h*6.5);
-  text('BONUS', width/11+15, h*6.5);
+  image(freccia, w, h*6, freccia.width / 6, freccia.height / 6);
 
 
-  //BARRA
-  fill('#D5D0D3'); //barre grige
-  rectMode(CENTER);
-  rect(w, h*7 ,width / 3.5, 15, 20); //rect(x,y,w,h,[tl])
-  xBarra = ((width / 3.5) / 100) * p_coord; //altezza barra %, xTot= 439 = width / 3.5
-  push();
-  rectMode(CORNER);
-  fill('#877B85');//barre viola
-  //width/7 è la metà della barra, che è lunga width/3.5
-  rect(w-width/7, h*7-7.5, xBarra ,15, 20);
-  pop();
+  //BARRA COORDINAZIONE
+  fill('#D5D0D3'); //griga
+rectMode(CENTER);
+rect(w*10, h*45.5, width / 3.5, 15, 20); //rect(x,y,w,h,[tl])
+xBarra = ((width / 3.5) / 100) * p_coord; //altezza barra %, xTot= 439 = width / 3.5
+push();
+rectMode(CORNER);
+fill('#877B85'); //viola
+//width/7 è la metà della barra, che è lunga width/3.5
+rect(w*10 - width / 7, h*45.5-7.5 , xBarra, 15, 20);
+pop();
 
 //pallini BONUS
   for(let i=0; i<6;i++){
-    ellipse(width/11 + s , h*7 , 15);
-    s =25*i;
+    ellipse(w + s, h*45.5, 15);
+    s = 25 * i;
     }
 /////////////////// LA PARTE SOPRA è STANDARD ///////////////////////////////////////////////
 //microfono
   let vol = round( mic.getLevel() , 2) *1000;
   //console.log('volume: ' +  vol);
 
-
 push();
-//scritte bottoni
+//CONTENITORI SCRITTE DA PRONUNCIARE
 rectMode(CENTER);
 stroke(textColorS)//viola
 strokeWeight(5);
 fill(bButtonColorS)//bianco
-rect(width/20*6,height/2,width/20*4,60,40);
+rect(w*6,height/2,w*4,60,40);
 stroke(textColorD)//viola
 strokeWeight(5);
 fill(bButtonColorD)//bianco
-rect(width/20*14,height/2,width/20*4,60,40);
+rect(w*14,height/2,w*4,60,40);
 
 noStroke();
 textSize(30);
 textAlign(CENTER,TOP);
 fill(textColorS)//viola
-text('YEAH',width/20*6,height/2-15);
+text('YEAH',w*6,height/2-15);
 fill(textColorD)//viola
-text('GOOD',width/20*14,height/2-15);
+text('GOOD',w*14,height/2-15);
 pop();
 
 //ritmo
@@ -153,61 +153,46 @@ push();
 if(i<2 || i==2 ){
 document.getElementById("tutorial").style.display= "block";
 //image(tut1Icon, width / 2, height / 2, tut1Icon.width/3.5, tut1Icon.height/3.5);
-text('TUTORIAL', width /20*10, height / 6*3.7);
-text('Esulta con una parola', w, height / 6*3.5);
+text('TUTORIAL', w*10, height / 6*3.7);
+text('Esulta con una parola',w*10, height / 6*3.5);
 }else{
 document.getElementById("tutorial").style.display= "none";
 }
 pop();
-  //barrette suono
-  for (var x = width / 6 * 1.5; x < width / 2.2; x += 40) {
-    if (i % 2 != 0) { //quando i è dispari altezza deve diventare 1*random
-      alt = 1 * random(2, 10);
-      n_trombetta++;
-    } else {
-      alt = 1;
-      n_trombetta = 0.1;
-    }
-    //liniette suono della trombetta
-    // noStroke();
-    // fill(135, 123, 133);
-    // rectMode(CENTER);
-    // rect(x, height / 2, 15, 15 * alt, 20);
-    // rect(x + width / 3.15, height / 2, 15, 15 * alt, 20);
-  }
 
   //PERCENTUALE
-    text(p_coord + '%',w+(width/28), h*6.5 ); //w, height / 5 * 4.5
-
-  //PER LA BARRA DELLA PERCENTUALE
-  //interazione utente, temporaneamente tasto ENTER
-  if (alt != 1 & keyIsDown(ENTER)) {
-    n_interazione += 4; // per far tornare la percentuale in pai alla trombetta
-    p_coord = round((n_interazione / n_trombetta) * 100) * 2;
-  } else if (alt = 1) {
-    n_interazione = 0;
+  if (keyIsDown(ENTER)) {
+    p_coord = round(random(2,80));
+  } else {
     p_coord = 0;
   }
 
+push();
+  textAlign(CORNER);
+  fill('#B7AEB5'); //3° PALETTE
+  text('COORDINAZIONE  ' + p_coord + ' %', w*10, h*43);
+pop();
 
 
-//ICONE NORMALI
-if (vol>0 && i>2) {
-    push();//tasto scuro attivo
-    fill('#877B85');
-    noStroke();
-    strokeWeight(5);
-    ellipse(width / 2, height / 2, 100); //cerchio centrale
-    image(baloonBIcon, width / 2, height / 2, baloonBIcon.width / 1.5, baloonBIcon.height / 1.5);
-    pop();
-  }else if (i>2 ){ // cambio colore dle bottone centrale: feedback utente
-  push();
-  noFill();
-  stroke('#877B85');
-  strokeWeight(5);
-  ellipse(width / 2, height / 2, 100); //cerchio centrale
-  image(baloonIcon, width / 2, height / 2, baloonIcon.width / 1.5, baloonIcon.height / 1.5); // trombetta scura
-  pop();}
+    //ICONA CENTRALE CHE REAGISCE AL MIC
+    if (vol>0 && i>2) {
+        push();//tasto scuro attivo
+        fill('#877B85');
+        noStroke();
+        strokeWeight(5);
+        ellipse(width / 2, height / 2, 100); //cerchio centrale
+        image(baloonBIcon, width / 2, height / 2, baloonBIcon.width / 1.5, baloonBIcon.height / 1.5);
+        pop();
+      }else if (i>2 ){ // cambio colore dle bottone centrale: feedback utente
+      push();
+      noFill();
+      stroke('#877B85');
+      strokeWeight(5);
+      ellipse(width / 2, height / 2, 100); //cerchio centrale
+      image(baloonIcon, width / 2, height / 2, baloonIcon.width / 1.5, baloonIcon.height / 1.5); // trombetta scura
+      pop();
+      }
+
 }
 
 function windowResized() {
